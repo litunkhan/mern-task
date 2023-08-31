836f23ac49c1c21d299e92d8eb26afa3d9b47605
litunkhan <litunkhan6@gmail.com>
2023-08-31 17:19:02 +0600

    deployed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect((error) => {
+     client.connect((error) => {
       if (error) {
         console.error(error);
         return;
